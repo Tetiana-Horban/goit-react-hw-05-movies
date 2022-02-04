eafaf38d3a42b84692b0491f71faf2d215e98a6e
Tetiana-Horban <tanya.ryptyk@gmail.com>
2022-02-05 01:52:13 +0200

    Update MovieDetailsPage.jsx

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,17 +1,25 @@
 import { useEffect, useState } from 'react';
 import { fetchMovieDetails } from '../../services/api';
-import { Outlet, useParams } from 'react-router-dom';
+import { Outlet, useParams, useLocation, useNavigate } from 'react-router-dom';
 import MovieDetailsCard from '../../components/MovieDetailsCard/MovieDetailsCard';
 import { NavLink, List, Item } from './MovieDetailsPage.styled';
+import ButtonBack from '../../components/ButtonBack/ButtonBack';
 
 const MovieDetailsPage = () => {
+  const location = useLocation();
+  console.log(location);
+  const navigate = useNavigate();
   let { idMovie } = useParams();
-  const [detailsMovie, setDetailsMovie] = useState([]);
+  const [detailsMovie, setDetailsMovie] = useState({});
+  let from = location.state?.from?.pathname || '/';
+
   useEffect(() => {
     const getDetailsMovie = () => {
-      fetchMovieDetails(idMovie).then(movie => {
-        setDetailsMovie(movie);
-      });
+      fetchMovieDetails(idMovie)
+        .then(movie => {
+          setDetailsMovie(movie);
+        })
+        .catch(error => console.error('Error:', error));
     };
     getDetailsMovie();
   }, [idMovie]);
@@ -23,14 +31,26 @@ const MovieDetailsPage = () => {
         <List>
           Additional information
           <Item>
-            <NavLink to="cast">Cast</NavLink>
+            <NavLink to={{ pathname: 'cast' }} state={{ from: location }}>
+              Cast
+            </NavLink>
           </Item>
           <Item>
-            <NavLink to="reviews">Reviews</NavLink>
+            <NavLink to={{ pathname: 'reviews' }} state={{ from: location }}>
+              Reviews
+            </NavLink>
           </Item>
         </List>
         <Outlet />
       </div>
+      <ButtonBack
+        onClick={() => {
+          if (!location.state) {
+            navigate(-1);
+          }
+          navigate(from);
+        }}
+      />
     </div>
   );
 };
