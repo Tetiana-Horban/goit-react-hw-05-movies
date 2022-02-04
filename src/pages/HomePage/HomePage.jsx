86f8df9a63aa059aa4d45584651828fc7dcb18a4
Tetiana-Horban <tanya.ryptyk@gmail.com>
2022-02-05 01:52:23 +0200

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,16 +1,24 @@
 import { fetchTrendingMovies } from '../../services/api';
 import { useEffect, useState } from 'react';
+import PageHeading from '../../components/PageHeading/PageHeading';
 import MoviesHomeList from '../../components/MoviesHomeList/MoviesHomeList';
 
 const HomePage = () => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    fetchTrendingMovies().then(movies => {
-      setMovies(movies.results);
-    });
+    fetchTrendingMovies()
+      .then(movies => {
+        setMovies(movies.results);
+      })
+      .catch(error => console.error('Error:', error));
   }, []);
 
-  return <MoviesHomeList movies={movies} />;
+  return (
+    <>
+      <PageHeading title={'Tranding today'} />
+      <MoviesHomeList movies={movies} />
+    </>
+  );
 };
 export default HomePage;
